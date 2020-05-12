4f0b45db7b56158419bf59b84c2cbde145bad905
Philip Roush <roush.philip@gmail.com>
2020-05-12 19:37:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,6 +10,7 @@
                 steals: 3,
                 blocks: 1,
                 slam_dunks: 1 },
+                
               { player_name: 'Reggie Evans',
                 number: 30,
                 shoe: 14,
@@ -19,6 +20,7 @@
                 steals: 12,
                 blocks: 12,
                 slam_dunks: 7 },
+                
               { player_name: 'Brook Lopez',
                 number: 11,
                 shoe: 17,
@@ -28,6 +30,7 @@
                 steals: 3,
                 blocks: 1,
                 slam_dunks: 15 },
+                
               { player_name: 'Mason Plumlee',
                 number: 1,
                 shoe: 19,
@@ -37,6 +40,7 @@
                 steals: 3,
                 blocks: 8,
                 slam_dunks: 5 },
+                
               { player_name: 'Jason Terry',
                 number: 31,
                 shoe: 15,
