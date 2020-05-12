e5e9ebfa028977c0d9db5c1b2a1af3417cff0443
Philip Roush <roush.philip@gmail.com>
2020-05-12 19:48:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     away: { team_name: 'Charlotte Hornets',
             colors: %w[Turquoise Purple],
             players: [
-              { player_name: 'Jeff Adrien',
+              { player_name: "Jeff Adrien",
                 number: 4,
                 shoe: 18,
                 points: 10,
